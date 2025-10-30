dd90e368de03c66f3345ecc4040f57827e4d206d
chacha86 <taejincha0619@gmail.com>
2025-10-30 09:50:57 +0900

    exam4

---
+++ src/main/kotlin/day1/exam4/Main.kt
@@ -0,0 +1,11 @@
+package day1.exam4
+
+fun main(args: Array<String>) {
+    for(i in 0 until  5) {
+        println("Count: ${i}")
+    }
+
+    for(i in 0 ..  4) {
+        println("Count: ${i}")
+    }
+}
