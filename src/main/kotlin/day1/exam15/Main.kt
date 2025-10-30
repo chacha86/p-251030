3c27de958b392847b591e732015df13be27da927
chacha86 <taejincha0619@gmail.com>
2025-10-30 12:06:47 +0900

    exam15

---
+++ src/main/kotlin/day1/exam15/Main.kt
@@ -0,0 +1,9 @@
+package day1.exam15
+
+fun main() {
+    val name = "Alice"
+    val age = 25
+
+//    println("${name.length}") // 문자열 템플릿 내부에서 표현식 사용
+    println("안녕하세요 저는 ${name}입니다. 나이는 ${age}살입니다.")
+}
