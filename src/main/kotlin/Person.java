028ed2f1ce213f58d0a6b3dd43a0fe56343880a6
chacha86 <taejincha0619@gmail.com>
2025-10-30 09:25:08 +0900

    exam1

---
+++ src/main/kotlin/Person.java
@@ -0,0 +1,15 @@
+public class Person {
+
+    int age;
+    String name;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello, my name is " + name + " and I am " + age + " years old.");
+    }
+
+}
