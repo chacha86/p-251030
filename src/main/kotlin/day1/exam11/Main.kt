d52ed19e4de4e2c0265d25f1b644f2ef3a6dcbb7
chacha86 <taejincha0619@gmail.com>
2025-10-30 11:15:34 +0900

    exam11

---
+++ src/main/kotlin/day1/exam11/Main.kt
@@ -0,0 +1,16 @@
+package day1.exam11
+
+data class Person(
+    val name: String,
+    val age: Int
+) {
+
+}
+
+fun main() {
+    val p1 = Person("Alice", 29)
+    val p2 = Person("Alice", 29)
+
+    println(p1)  // 자동 생성된 toString() 메서드 사용
+    println(p1 == p2) // 자동 생성된 equals() 메서드 사용. ==로 사용해도 된다.
+}
